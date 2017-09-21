0fe79c29605522fc8194ecd1bd7e3cb5db50a153
Mai Hoang Viet <mhviet@TMA.COM.VN>
2017-09-21 15:18:53 +0700

    remove redundant file

--- app/services/db/index.js
+++ app/services/db/index.js
@@ -1,10 +1,8 @@
 // Common
-const initFindById = require('./findById');
 const initInsertGenId = require('./insertGenId');
 
 module.exports = function init(db) {
   return {
-    findById: initFindById(db),
     insertGenId: initInsertGenId(db),
   };
 };
